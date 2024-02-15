824e00d715b3697599b230214c934d8b1de6fd49
Jan <chmelikjan@atlas.cz>
2024-02-15 10:02:07 +0100

    starting html structure set

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,39 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-info-subtle vw-100 vh-100">
-      <div className="container bg-warning-subtle"></div>
+      {/* //#region container */}
+      <div className="container bg-warning-subtle">
+        {/* #region wrapping row */}
+        <div className="row">
+          {/* //#region 1stcol */}
+          <div className="col-6"></div>
+          {/* //#endregion 1stcol */}
+          {/* //#region  2ndcol */}
+          <div className="col-6">
+            {/* //#region 1st in row */}
+            <div className="row">
+              <div className="col-6"></div>
+              <div className="col-6"></div>
+            </div>
+            {/* //#endregion 1st in row */}
+            {/* //#region 2nd in row */}
+            <div className="row">
+              <div className="col-6"></div>
+              <div className="col-6"></div>
+            </div>
+            {/* //#endregion 2nd in row */}
+            {/* //#region 3rd in row */}
+            <div className="row">
+              <div className="col-6"></div>
+              <div className="col-6"></div>
+            </div>
+            {/* //#endregion 3rd in row */}
+          </div>
+          {/* //#endregion 2ndcol */}
+        </div>
+        {/* //#endregion wrapping row */}
+      </div>
+      {/* //#endregion container */}
     </div>
   );
 }
